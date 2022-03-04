f0ec26e990e9fa0ecc2da10542ea3e319646c29a
Priyanka <original.manaria@gmail.com>
2022-03-04 11:46:38 +0530

    toggle theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import Article from "./components/Article";
 import data from "./components/Data";
 
 function App() {
+  const [theme, setTheme] = useState("light-theme");
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn">toggle</button>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
         </div>
       </nav>
       <section className="articles">
